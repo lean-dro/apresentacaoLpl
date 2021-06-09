2cee57235bb42b7935e68483e9de13c508bb3044
Thiago M <67590378+Thigos@users.noreply.github.com>
2021-06-08 22:17:34 -0300

    Animação Slide Adicionada

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 // index JS
+// Vars
+divAnim = document.getElementById("divAnim")
+btForm = document.getElementById("btForm")
 
-//Vars
-
+// onClick
+btForm.onclick = function(){
+    slideAnim()
+    //Esperar 1s de anim e abrir a página do forms
+    /*setTimeout(function(){
+        window.location.href = "forms"
+    },1000)*/
+}
 
 // Func Anim
-
+function slideAnim(){
+    divAnim.style.transform = 'translateX(100%)'
+}
